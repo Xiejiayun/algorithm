92c94f4b71f514918b9ec160cbc210063805ae7b
hzxiejiayun <furnace09@gmail.com>
2018-03-20 14:17:59 +0800

    二进制中1的个数，去掉累赘代码

--- leetcode/src/main/java/com/netease/algorithm/bit/CountBinaryOnes.java
+++ leetcode/src/main/java/com/netease/algorithm/bit/CountBinaryOnes.java
@@ -6,9 +6,6 @@ package com.netease.algorithm.bit;
 public class CountBinaryOnes {
 
     public int countBinaryOnes(int num) {
-        if (num == 0) {
-            return 0;
-        }
         int count = 0;
         while (num != 0) {
             num = num & (num-1);
